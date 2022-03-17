fe95e10e3a700ab6f6130a53df4190bf4866608e
Samosathree <84139052+Samosathree@users.noreply.github.com>
2022-03-17 18:46:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
-function preload(){
+
+function preload() {
 }
- 
-function setup(){
-    canvas = createCanvas(640, 480);
-    canvas.center();
+
+function setup() {
+  canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
     video = createCapture(VIDEO);
     video.hide();
-
-    ctx.beginPath();
-    ctx.strokeStyle = "blue";
-    ctx.lineWidth = 4;
-    ctx.arc(250, 210, 40, 0, 2 * Math.PI);
-    ctx.stroke();
+  background(300);
+    noFill();
+beginShape();
+vertex(50, 20);
+vertex(600, 20);
+vertex(600, 450);
+vertex(50, 450);
+endShape(CLOSE);
+  fill(100, 0, 192, 127);
+  stroke(127, 63, 120);
+circle(50, 30, 50);
+ circle(50, 450, 50);
+  circle(600, 30, 50);
+   circle(600, 450, 50);
 }
- 
-function draw(){
+
+function draw() {
     image(video, 0, 0, 640, 480);
+   
 }
- 
-function take_snapshot(){
-    save('HappyBday.png');
-}
- 
 
+function take_snapshot(){
+save('HOIYA.png');
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+
+}
+
+function take_snapshot(){
+save('student_name.png');
+}
